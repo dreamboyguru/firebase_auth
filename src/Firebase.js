daa3b73b9d0590f7db5bdbdd289701f745f59263
Gurusidda Hanamannavr <143526836+dreamboyguru@users.noreply.github.com>
2024-03-06 13:43:49 +0530

    Update Firebase.js
    test

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+console.log(process.env.REACT_APP_apiKey);
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_apiKey,
